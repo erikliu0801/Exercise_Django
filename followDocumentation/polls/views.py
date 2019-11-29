8934496a1c61ba3d90b7b694b4235c776cf0c30c
Erik <erikliu0801@gmail.com>
2019-11-29 21:19:14 +0800

    add polls function

--- followDocumentation/polls/views.py
+++ followDocumentation/polls/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello world, It's at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse('Here is the question %s.' %question_id)
+
+def results(request, question_id):
+    response = "Here is the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+    
