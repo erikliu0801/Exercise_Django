0cf486a0f33fee5855d08904835421eb32bf4b17
Erik <erikliu0801@gmail.com>
2019-12-02 14:15:15 +0800

    add search fields

--- followDocumentation/polls/models.py
+++ followDocumentation/polls/models.py
@@ -12,6 +12,7 @@ class Question(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
+    search_fields = ['question_text']
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
