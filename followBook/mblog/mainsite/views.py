0092a2b4712061b0437079847a02d66340c3f6aa
Erik <erikliu0801@gmail.com>
2019-12-03 18:42:26 +0800

    add homepage

--- followBook/mblog/mainsite/views.py
+++ followBook/mblog/mainsite/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from datetime import datetime
 
 # Create your views here.
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_list = list()
+#     for count, post in enumerate(posts):
+#         post_list.append("No.{}:".format(str(count))+str(post)+"<br>")
+#         post_list.append("<small>"+str(post.body.encode('utf-8'))+"</small><br><br>")
+#     return HttpResponse(post_list)
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html',locals())
