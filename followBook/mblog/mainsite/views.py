0cc8c15549749824e5418675f00aa8961e7ef3fd
Erik <erikliu0801@gmail.com>
2019-12-05 14:12:41 +0800

    add post template

--- followBook/mblog/mainsite/views.py
+++ followBook/mblog/mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 from datetime import datetime
@@ -16,3 +16,11 @@ def homepage(request):
     posts = Post.objects.all()
     now = datetime.now()
     return render(request, 'index.html',locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug=slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
